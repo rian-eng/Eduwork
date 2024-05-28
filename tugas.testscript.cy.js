ea668b9c312391dbd19828f78dbd6de46ccc27c1
rian-eng <rianmaulana.eng@gmail.com>
2024-05-28 18:43:31 +0700

    Add files via upload

---
+++ tugas.testscript.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('My First Test', ()=> {
+    it('clicking "type" show the right heading', () => {
+        cy.visit('https://example.cypress.io')
+
+        cy.pause()
+
+        cy.contains('type').click()
+
+        cy.url().should('include', '/commands/actions')
+
+        cy.get('.action-email')
+          .type('fake@email.com')
+          .should('have.value', 'fake@email.com')
+    });
+});
