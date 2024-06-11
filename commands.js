1d7a1ed80d7e61e6b428e85c8f7e7e13fbbeec34
rian-eng <rianmaulana.eng@gmail.com>
2024-06-11 16:29:48 +0700

    Add files via upload

--- commands.js
+++ commands.js
@@ -1,7 +1,6 @@
-// file ini terdapat pada folder support yang dipanggil pada File
-// "tugas custom command.cy.js"
+// file ini terdapat pada folder support yang dipanggil pada File "tugas login&logout.cy.js"
 
-Cypress.Commands.add('tugas_custom_command', (username,password) => { // untuk file "tugas custom command.cy.js"
+Cypress.Commands.add('tugas_login_logout', (username,password) => { // untuk file "tugas tugas login&logout.cy.js"
     cy.clearCookies()
     cy.clearAllLocalStorage()
     
@@ -14,20 +13,4 @@ Cypress.Commands.add('tugas_custom_command', (username,password) => { // untuk f
     //cy.get('#user_remember_me').click()
             
     cy.get('input[name="submit"]').click()
-            
-    cy.url().should('include', '/bank/account-summary')
-
-    cy.contains('Pay Bills').click()
-    cy.url().should('include', '/bank/pay-bills',)
-            
-    cy.get('#sp_amount').clear()
-    cy.get('#sp_amount').type('100')
-
-    cy.get('#sp_description').clear()
-    cy.get('#sp_description').type('make payment')
-
-    cy.get('#sp_date').clear()
-    cy.get('#sp_date').type('2024-6-3')
-
-    cy.get('input[class="btn btn-primary"]').click()
-}) 
+})
